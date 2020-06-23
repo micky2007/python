72b5122883494aa5bd4646546767854461cb7cfa
Michaela <michaelakovan@gmail.comgit config --global user.name Michaela>
2020-06-22 22:06:25 -0400

    my first python programs

---
+++ math.py
@@ -0,0 +1,45 @@
+def add(x, y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x/y
+
+def average(x, y):
+    sum = add(x,y)
+    return sum/2
+
+
+while True:
+    choice = input ("enter choice 1/2/3/4/5:")
+    print (choice)
+
+    if choice in ('1', '2', '3', '4', '5'):
+        num1 = float(input("enter first number: "))
+        num2 = float(input("enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2) )
+
+        if choice == '2':
+            print(num1, '-', num2, '=', subtract(num1, num2)  )
+
+        if choice == '3':
+            print(num1, '*', num2, '=', multiply(num1, num2) )
+
+        if choice == '4':
+            print(num1, '/', num2, '=', divide(num1, num2))
+
+        if choice == '5':
+            print("average of", num1, num2, '=', average(num1, num2) )
+
+
+
+
+    else:
+        print("invalid input")
