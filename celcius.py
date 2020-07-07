71c057fe552e09cc1639035511bf0396f736a76f
Michaela <michaelakovan@gmail.comgit config --global user.name Michaela>
2020-07-07 00:17:09 -0400

    more coding

--- celcius.py
+++ celcius.py
@@ -5,11 +5,13 @@ def convertCtoF(celcius):
 celcius = 30
 farenheight = celcius * 1.8 + 32
 
-while true:
-    choice = input ("enter c/f)
+while True:
+    choice = input ("enter c/f: ")
+    print (choice)
 
-if choice in ('c,', 'f'):
-    num1 = float(input("enter number: "))
+    if choice in ('c', 'f'):
 
-if choice == 'c':
-    print(convertCtoF(num1)))
+
+        num1 = float(input("enter number: "))
+        if choice == 'c':
+            print(convertCtoF(num1))
--- math.py
+++ math.py
@@ -14,12 +14,23 @@ def average(x, y):
     sum = add(x,y)
     return sum/2
 
+def max(x,y):
+    if x > y:
+        return x
+    if y > x:
+        return y
+
+def min(x,y):
+    if x < y:
+        return x
+    if y < x:
+        return y
 
 while True:
-    choice = input ("enter choice 1/2/3/4/5:")
+    choice = input ("enter choice 1/2/3/4/5/6/7:")
     print (choice)
 
-    if choice in ('1', '2', '3', '4', '5'):
+    if choice in ('1', '2', '3', '4', '5', '6', '7'):
         num1 = float(input("enter first number: "))
         num2 = float(input("enter second number: "))
 
@@ -38,6 +49,11 @@ while True:
         if choice == '5':
             print("average of", num1, num2, '=', average(num1, num2) )
 
+        if choice == '6':
+            print("max number is", num1, '>', num2, '=', max(num1, num2) )
+
+        if choice == '7':
+            print("min number is", num1, '<', num2, '=', min(num1, num2) )
 
 
 
