874d01f77c7aff697a156c0c0e1b50ca9458df75
Michaela <michaelakovan@gmail.comgit config --global user.name Michaela>
2020-07-07 23:18:39 -0400

    playing with lists

---
+++ lists.py
@@ -0,0 +1,40 @@
+print("learning python lists")
+
+
+fruitlist = ["apple", "bannana", "cherry", "strawberry"]
+print(fruitlist)
+
+for fruit in fruitlist:
+    print("fruit")
+
+numberlist = [100, 200, 300, 4000, 1000]
+print(numberlist)
+total = 0
+for number in numberlist:
+    print(number)
+    total = total + number
+
+print(total)
+
+newlist = [4, 5, 6, 7, 8]
+print(newlist)
+
+sum = 0
+average = 0
+count = 0
+
+for number in newlist:
+    sum = sum + number
+    if(count == 0):
+        min = number
+    elif (number < min):
+        min = number
+    count = count + 1
+
+
+print(sum)
+average = sum/len(newlist)
+print(average)
+print(min)
+
+
